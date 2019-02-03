ae0e8835c58840c43f10478aa092984d67a4ab33
JoshVanL <vleeuwenjoshua@gmail.com>
2019-02-03 13:11:38 +0000

    Re-use print code for operators
    Signed-off-by: JoshVanL <vleeuwenjoshua@gmail.com>

--- interpreter/ast/operators/print.go
+++ interpreter/ast/operators/print.go
@@ -9,23 +9,46 @@ import (
 )
 
 func Print(in io.ReadCloser, args []string) (func() error, error) {
+	return writeToFile(in, args, os.O_CREATE|os.O_WRONLY, true)
+}
+
+func Append(in io.ReadCloser, args []string) (func() error, error) {
+	return writeToFile(in, args, os.O_APPEND|os.O_CREATE|os.O_WRONLY, false)
+}
+
+func writeToFile(in io.ReadCloser, args []string, flags int, remove bool) (func() error, error) {
 	if len(args) < 1 {
 		return nil, errors.MissingExpression
 	}
 
 	return func() error {
-		f, err := os.OpenFile(args[0], os.O_CREATE|os.O_WRONLY, 0644)
+		perm := os.FileMode(0644)
+		info, err := os.Stat(args[0])
+		if err == nil {
+			perm = info.Mode()
+
+			if remove {
+				err = os.Remove(args[0])
+				if err != nil {
+					return err
+				}
+			}
+		}
+
+		f, err := os.OpenFile(args[0], flags, perm)
 		if err != nil {
 			return err
 		}
 
 		if _, err := io.Copy(f, in); err != nil {
+			f.Close()
 			return err
 		}
 
 		if len(args) > 1 {
 			_, err = f.Write([]byte(strings.Join(args[1:], " ")))
 			if err != nil {
+				f.Close()
 				return err
 			}
 		}
